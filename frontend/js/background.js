61a57a7b93b70dd2f7e1eac4b4bd22967bd7d00a
= <=>
2021-07-06 10:45:42 +0100

    first commit

--- frontend/js/background.js
+++ frontend/js/background.js
@@ -4,7 +4,7 @@ var isPhish = {};
 
 
 function fetchLive(callback) {
-  $.getJSON("https://raw.githubusercontent.com/picopalette/phishing-detection-plugin/master/static/classifier.json", function(data) {
+  $.getJSON("https://github.com/essentric-coder/ml_plugin/blob/main/static/classifier.json", function(data) {
       chrome.storage.local.set({cache: data, cacheTime: Date.now()}, function() {
           callback(data);
       });
